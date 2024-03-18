ddd3876f193baecd0478ab8ccbf6dd72fe314ae0
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-17 23:23:34 -0700

    adding

--- console.py
+++ console.py
@@ -113,19 +113,38 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+    def do_create(self, arg):
+        """Create a new instance of a specified class"""
+        if not arg:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args = arg.split()
+        class_name = args[0]
+        if class_name not in self.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        class_attrs = self.classes[class_name]
+        kwargs = {}
+        for pair in args[1:]:
+            if '=' in pair:
+                key, value = pair.split('=', 1)
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1].replace('_', ' ').replace('\\"', '"')
+                elif '.' in value:
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        continue
+                else:
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        continue
+                kwargs[key] = value
+        new_instance = class_attrs(**kwargs)
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
-
+                        
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
